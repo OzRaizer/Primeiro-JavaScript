9305c088d1a7a6d548725c10482c6c803910864d
Raizer <104086188+OzRaizer@users.noreply.github.com>
2024-06-12 21:54:38 -0300

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nome = prompt("Informe seu nome:")
+const sobrenome = prompt("Informe seu sobrenome:")
+let anoAtual = prompt("Insira o ano que você está:")
+let idadeAtual = prompt("Insira sua idade atual:")
+
+
+console.log(`Prazer ${nome} ${sobrenome}, de ${idadeAtual}, vejo que você nasceu em ${idadeAtual-anoAtual}`)
+
+alert(`Prazer ${nome} ${sobrenome}, de ${idadeAtual}, vejo que você nasceu em ${idadeAtual-anoAtual}`)
